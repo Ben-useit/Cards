756bfd342934ba751692c91f4348eae5b8ff2b54
Ben <mail@useit-mw.com>
2025-01-17 12:38:52 +0200

    Enabled protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isPublicRoutes = createRouteMatcher(['/', '/about']);
+
+export default clerkMiddleware(async (auth, req) => {
+  console.log(isPublicRoutes(req));
+  if (!isPublicRoutes(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
